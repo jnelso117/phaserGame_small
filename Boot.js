1ceef565a09410d28492e7c3ccde472a4bd680e5
Jeremy Carter Nelson <jcn1990s@gmail.com>
2014-11-26 01:18:05 -0600

    Phaser Basic Game
    For work.  We should use it!

--- Boot.js
+++ Boot.js
@@ -1,37 +1,29 @@
 var BasicGame = {};
 
-BasicGame.Boot = function (game) {
-
-};
+BasicGame.Boot = function (game) {this.game = game},
 
 BasicGame.Boot.prototype = {
-    init: function () {
-
-        //  Unless you specifically know your game needs to support multi-touch I would recommend setting this to 1
-        this.input.maxPointers = 1;
-
-        //  Phaser will automatically pause if the browser tab the game is in loses focus. You can disable that here:
-        this.stage.disableVisibilityChange = true;
-
-        //  This tells the game to resize the renderer to match the game dimensions (i.e. 100% browser width / height)
-        this.scale.scaleMode = Phaser.ScaleManager.RESIZE;
-
+    preload : function () {
+        this.load.image('sky', 'assets/sky.png');
+        this.load.image('firstaid', 'assets/firstaid.png');
+        this.load.image('diamond', 'assets/diamond.png');
     },
-
-    preload: function () {
-
-        //  Here we load the assets required for our preloader (in this case a background and a loading bar)
-        this.load.image('preloaderBackground', 'images/preloader_background.jpg');
-        this.load.image('preloaderBar', 'images/preloadr_bar.png');
-
-    },
-
+    
     create: function () {
+    this.input.maxPointers = 1;
+    this.stage.disableVisibilityChange = true;
+    this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+    this.scale.minWidth = 600;
+    this.scale.minHeight = 400;
+    this.scale.pageAlignHorizontally = true;
+    this.scale.pageAlignVertically = true;
+    this.stage.forcePortrait = true; //Force landscape in browser
+    this.scale.setScreenSize(true);
+    
+    this.input.addPointer(); //correspond to the 1 pointer
+    this.stage.backgroundColor = '#121643'; 
+    this.state.start('Preloader'); //starts the Preloader.js file
+}
+}
 
-        //  By this point the preloader assets have loaded to the cache, we've set the game settings
-        //  So now let's start the real preloader going
-        this.state.start('Preloader');
-
-    }
-
-};
+    
